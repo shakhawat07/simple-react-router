7382f066ebee161533b62285b6c29c0d43530223
shakhawat07 <shakhawathossainmridha@gmail.com>
2021-10-02 23:18:37 +0600

    router added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home/Home';
 import Friends from './components/Friends/Friends';
 import About from './components/About/About';
 import NotFound from './components/NotFound/NotFound';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
-      <Friends></Friends>
-      <About></About>
-      <NotFound></NotFound>
+      <Router>
+        <Switch>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route path="/about">
+            <About></About>
+          </Route>
+          <Route path="/friends">
+            <Friends></Friends>
+          </Route>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/components/Friends/Friends.js
+++ src/components/Friends/Friends.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Friend from '../Friend/Friend';
+import './Friends.css'
 
 const Friends = () => {
+    const [friends, setFriends] = useState([]);
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(res => res.json())
+            .then(data => setFriends(data))
+    }, [])
     return (
         <div>
-            <h1>Friends</h1>
+            <h1>Friends = {friends.length}</h1>
+            <div className="friend-container">
+                {
+                    friends.map(friend => <Friend
+                        key={friend.id}
+                        friend={friend}
+                    ></Friend>)
+                }
+            </div>
         </div>
     );
 };

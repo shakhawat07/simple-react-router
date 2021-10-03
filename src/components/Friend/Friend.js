d54dd7fcd0d5172f006f324b1e654538d8a6722d
shakhawat07 <shakhawathossainmridha@gmail.com>
2021-10-03 12:29:42 +0600

    friend.js updated

--- src/components/Friend/Friend.js
+++ src/components/Friend/Friend.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 const Friend = (props) => {
     const { id, name, phone, website, address } = props.friend;
+    const history = useHistory();
     const friendStyle = {
         border: '3px solid goldenrod',
         margin: '10px',
         padding: '10px',
         borderRadius: '10px'
+    }
+    const handleFriendClick = () => {
+        history.push(`/friend/${id}`);
     }
     const url = `/friend/${id}`
     return (
@@ -17,6 +21,12 @@ const Friend = (props) => {
             <p>Visit me: {website}</p>
             <p>I live in: {address.city}</p>
             <Link to={url}>Visit Me</Link>
+            <br />
+            <Link to={url}>
+                <button>Visit Me</button>
+            </Link>
+            <br />
+            <button onClick={handleFriendClick}>Visit Me 2</button>
         </div>
     );
 };
--- src/components/FriendDetail/FriendDetail.js
+++ src/components/FriendDetail/FriendDetail.js
@@ -1,16 +1,21 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router';
+import { useHistory, useParams } from 'react-router';
 
 const FriendDetail = () => {
     const { friendId } = useParams();
     // console.log(friendId);
+    const history = useHistory();
     const [friend, setFriend] = useState({});
     useEffect(() => {
         const url = `https://jsonplaceholder.typicode.com/users/${friendId}`
         fetch(url)
             .then(res => res.json())
             .then(data => setFriend(data));
-    }, [])
+    }, []);
+
+    const handleClick = () => {
+        history.push(`/friends`)
+    }
     return (
         <div>
             <h1>Friend detail {friendId}</h1>
@@ -18,6 +23,7 @@ const FriendDetail = () => {
             <h3>{friend.phone}</h3>
             <h4>{friend.website}</h4>
             <h4>{friend.company?.name}</h4>
+            <button onClick={handleClick}>See all friends</button>
         </div>
     );
 };
